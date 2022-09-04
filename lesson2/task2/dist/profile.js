fc8b4fa8909b31fa66d232ac103c7a45b709deed
Nadi123ya <nadezhda.bdarenko.96@gmail.com>
2022-09-04 18:21:00 +0400

    commited

--- lesson2/task2/dist/profile.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-// file path: src/profile.js
-var printProfile = function printProfile(profileData) {
-  var name = profileData.name,
-      company = profileData.company;
-  console.log("".concat(name, " from ").concat(company));
-};
